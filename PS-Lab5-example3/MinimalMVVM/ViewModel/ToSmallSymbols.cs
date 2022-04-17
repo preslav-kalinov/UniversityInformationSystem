726f97144426a9b2737ea411881c450b547b66f9
preslav-kalinov <preslav_00_k@abv.bg>
2022-04-18 01:37:10 +0300

    Exercise 5 (Update 2)

---
+++ PS-Lab5-example3/MinimalMVVM/ViewModel/ToSmallSymbols.cs
@@ -0,0 +1,47 @@
+﻿using MinimalMVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace MinimalMVVM.ViewModel
+{
+    class ToSmallSymbols : ObservableObject
+    {
+        protected readonly TextConverter _textConverter = new TextConverter(s => s.ToLower());
+        private string _someText;
+        private readonly ObservableCollection<string> _history = (ObservableCollection<string>)Presenter.History;
+
+        public string SomeText
+        {
+            get { return _someText; }
+            set
+            {
+                _someText = value;
+                RaisePropertyChangedEvent("SomeText");
+            }
+        }
+
+        public IEnumerable<string> History
+        {
+            get { return _history; }
+        }
+
+        public ICommand ConvertTextCommand
+        {
+            get { return new DelegateCommand(ConvertText); }
+        }
+
+        private void ConvertText()
+        {
+            AddToHistory(_textConverter.ConvertText(SomeText));
+            SomeText = String.Empty;
+        }
+
+        private void AddToHistory(string item)
+        {
+            if (!_history.Contains(item))
+                _history.Add(item);
+        }
+    }
+}
